096aab4dbd09ffed489918cf92b685499131187c
Mujakkir85 <mujakkirlucse@gmail.com>
2022-08-15 15:20:13 +0600

    get data from json file

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setProducts(data)
+            })
+    }, [])
+
     return (
         <div>
 
